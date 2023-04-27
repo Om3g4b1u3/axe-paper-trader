245a56dbe48f0d604d30c7aa67470985a89ee66f
milo <milo@alphaedge.ai>
2023-04-26 19:48:12 -0700

    Main updated

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,10 @@
+#> 'members' URL Configuration <#
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members),
+	path('', views.main, name='main'),
+	path('members/', views.members, name='members'),
+	path('members/member_details/<int:id>', views.member_details , name='member_details'),
+	path('testing/', views.testing, name='testing'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Member
 
-# Create your views here.
+def main(request):
+	template = loader.get_template('main.html')
+	return HttpResponse(template.render())
+
 def members(request):
-	return HttpResponse('Hello World!')
+	members = Member.objects.all().values()
+	template = loader.get_template('members.html')
+	context = {
+		'members': members
+	}
+	return HttpResponse(template.render(context, request))
+
+def member_details(request, id):
+	member = Member.objects.get(id=id)
+	template = loader.get_template('member_details.html')
+	context = {
+		'member': member,
+	}
+	return HttpResponse(template.render(context, request))
+
+def testing(request):
+	template = loader.get_template('template.html')
+	context = {
+		'fruits': ['Apple','Banana','Cherry'],
+	}
+	return HttpResponse(template.render(context, request))
